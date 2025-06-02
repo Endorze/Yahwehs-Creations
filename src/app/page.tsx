f38efe070af5cbc3097cb06c1d5422587d1d2dcf
Endorze <alexyurihallgren@hotmail.com>
2025-06-02 17:28:22 +0200

    feat: Added the playbutton to frontpage for people to test.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import PlayButton from "./components/PlayButton/PlayButton";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
+        <PlayButton />
         <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind! And goodmorning Vietnam!</h1>
     </div>
   );
