032ee5b3fb14f08646335f5cf9abc83801cb76b9
Endorze <alexyurihallgren@hotmail.com>
2025-06-02 17:15:19 +0200

    fix: Remoed import from page.tsx after finished testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import PlayButton from "./components/PlayButton/PlayButton";
-
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
