5cde86e0b83186460a7570104e393814158bc190
vineetha <vineethaveerapu4@gmail.com>
2025-06-03 11:10:01 +0200

    Button component to support  with href prop

--- src/app/components/testfolder/Button/index.tsx
+++ src/app/components/testfolder/Button/index.tsx
@@ -1,12 +1,18 @@
+import Link from "next/link";
+
 interface ButtonProps {
   text?: string;
+  href?: string;
 }
 
-const Button = ({ text = "Read more" }: ButtonProps) => {
+const Button = (props: ButtonProps) => {
+  const { text = "Read more", href = "/" } = props;
   return (
-    <div className="bg-[url('/images/orangebutton.png')] bg-cover bg-center bg-no-repeat text-xl text-white aspect-259/61 p-2 text-center hover:shadow-[0_0_10px_5px_rgba(255,165,0,0.5)] hover:scale-105 transition-all duration-300 cursor-pointer  font-bold  ">
-      {text}
-    </div>
+    <Link href={href}>
+      <div className="bg-[url('/images/orangebutton.png')] bg-cover bg-center bg-no-repeat text-xl text-white aspect-259/61 p-2 text-center hover:shadow-[0_0_10px_5px_rgba(255,165,0,0.5)] hover:scale-105 transition-all duration-300 cursor-pointer  font-bold  ">
+        {text}
+      </div>
+    </Link>
   );
 };
 
