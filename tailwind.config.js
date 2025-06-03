4464fe06a3a2ced5be9eb86c7662194ef2b24a4e
Moshira89 <mushiraaljak@example.com>
2025-06-03 09:19:20 +0200

    Add HeroSection component with background and button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{ts,tsx}",
-    "./src/pages/**/*.{ts,tsx}",
-    "./src/components/**/*.{ts,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
