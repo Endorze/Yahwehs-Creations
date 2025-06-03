701cf8a00b4a1618c1eabe4a76a48a9d04a86b3e
Endorze <alexyurihallgren@hotmail.com>
2025-06-03 15:48:14 +0200

    fix: Swapped backtick to normal tick in string

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,7 +3,7 @@ export default function BlogPage() {
     <section className="p-8">
       <h1 className="text-4xl font-bold mb-4">Blog Page</h1>
       <p className="text-lg text-gray-700">
-        Welcome to our Blog page. Here you’ll find information about our mission, team, and story.
+        Welcome to our Blog page. Here you'll find information about our mission, team, and story.
       </p>
     </section>
   );
