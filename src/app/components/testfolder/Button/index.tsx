0edccf077747e51c978a61aa9ba04f552e7748de
vineetha <vineethaveerapu4@gmail.com>
2025-06-03 11:33:47 +0200

    Update Button  styles

--- src/app/components/testfolder/Button/index.tsx
+++ src/app/components/testfolder/Button/index.tsx
@@ -6,12 +6,14 @@ interface ButtonProps {
 }
 
 const Button = (props: ButtonProps) => {
+  // This are default values for the button
   const { text = "Read more", href = "/" } = props;
   return (
-    <Link href={href}>
-      <div className="bg-[url('/images/orangebutton.png')] bg-cover bg-center bg-no-repeat text-xl text-white aspect-259/61 p-2 text-center hover:shadow-[0_0_10px_5px_rgba(255,165,0,0.5)] hover:scale-105 transition-all duration-300 cursor-pointer  font-bold  ">
-        {text}
-      </div>
+    <Link
+      href={href}
+      className="bg-[url('/images/orangebutton.png')] bg-cover bg-center bg-no-repeat text-xl text-white aspect-245/49 text-center hover:shadow-[0_0_10px_5px_rgba(255,165,0,0.5)] hover:scale-105 transition-all duration-300 cursor-pointer font-bold p-2 hover:bg-[rgba(255,165,0,0.5)]"
+    >
+      {text}
     </Link>
   );
 };
