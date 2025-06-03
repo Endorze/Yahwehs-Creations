20cc4f9f72c2543a9ebaeddc5848a766265db2cf
vineetha <vineethaveerapu4@gmail.com>
2025-06-03 07:36:14 +0200

    styles added

--- src/app/components/testfolder/Button/index.tsx
+++ src/app/components/testfolder/Button/index.tsx
@@ -4,7 +4,12 @@ interface ButtonProps {
 
 const Button = ({ text = "Read more" }: ButtonProps) => {
   return (
-    <div className="bg-[url('/images/orangebutton.png')] bg-cover bg-center bg-no-repeat  text-xl  text-white aspect-259/61 p-2 text-center ">
+    <div
+      className="bg-[url('/images/orangebutton.png')] bg-cover bg-center bg-no-repeat
+     text-xl text-white
+    aspect-259/61 p-2 text-center hover:shadow-[0_0_10px_5px_rgba(255,165,0,0.5)] hover:scale-105
+     transition-all duration-300 cursor-pointer  font-bold  "
+    >
       {text}
     </div>
   );
