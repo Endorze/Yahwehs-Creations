f86fb9385f262d8223784a7c899a7519d977c35e
Endorze <alexyurihallgren@hotmail.com>
2025-06-05 17:16:08 +0200

    Feat: added playtester section, reworked playbutton as it was broken

--- src/app/components/PlayButton/PlayButton.tsx
+++ src/app/components/PlayButton/PlayButton.tsx
@@ -2,7 +2,7 @@ const PlayButton = () => {
     return (
         <a href="https://futuregames.itch.io/yahwehs-creation" 
         target="_blank" 
-        aria-label="Play Yahweh's Creation"><img src={"/images/playfreebtn.png"} className="h-40"/></a>
+        aria-label="Play Yahweh's Creation"><img src={"/images/playfreebtn.png"} className="h-20"/></a>
     )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@ import PlaytestingSection from "./components/PlaytestingSection/PlaytestingSecti
 
 export default function Home() {
   return (
-    <div className="flex-col min-h-screen items-center justify-center bg-gray-100">
-      <PlaytestingSection />
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
         <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind! And goodmorning Vietnam!</h1>
     </div>
   );
