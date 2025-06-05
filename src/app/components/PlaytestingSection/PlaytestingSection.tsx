f7cc97387397ca0cfb500d8cc783df187b5752f5
Endorze <alexyurihallgren@hotmail.com>
2025-06-05 16:19:46 +0200

    pause again

--- src/app/components/PlaytestingSection/PlaytestingSection.tsx
+++ src/app/components/PlaytestingSection/PlaytestingSection.tsx
@@ -1,21 +1,24 @@
 import H2Title from "../H2Title/H2Title";
+import H3Title from "../H3Title/H3Title";
 import PlayButton from "../PlayButton/PlayButton";
 
 const PlaytestingSection = () => {
     return (
-        <div className="flex bg-[url('/images/playtesting/cathedralgameplay.png')] bg-cover bg-center h-128 w-full">
-            <div>
-                <H2Title text="Playtesting" size="96px"/>
-                <h3 className="text-white">NOW IN ALPHA</h3>
-                <h4>Early descent into Yahweh's Cathedral has begun...</h4>
-                <p>We’re currently looking for fearless souls to help test the game. Your feedback shapes the world — and perhaps your survival in it.</p>
-                <PlayButton />
-            </div>
-            <div>
-                <img src={"images/playtesting/image.png"}/>
-                <div className="flex">
-                    <p>Join our wonderful community on Discord</p>
-                    <img src={"images/playtesting/discord.png"} className="block w-20"/>
+        <div className="flex bg-[url('/images/playtesting/cathedralgameplay.png')] bg-cover bg-center h-128 w-full text-white justify-center">
+            <div className="flex flex-row items-center">
+                <div className="flex-1">
+                    <H2Title text="Playtesting" size="96px"/>
+                    <H3Title text="NOW IN ALPHA" size="48px" />
+                    <p>Early descent into Yahweh's Cathedral has begun...</p>
+                    <p>We’re currently looking for fearless souls to help test the game. Your feedback shapes the world — and perhaps your survival in it.</p>
+                    <PlayButton />
+                </div>
+                <div className="flex-1">
+                    <img src={"images/playtesting/image.png"} className="h-80"/>
+                    <div className="flex">
+                        <p>Join our wonderful community on Discord</p>
+                        <img src={"images/playtesting/discord.png"} className="block w-20"/>
+                    </div>
                 </div>
             </div>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import PlaytestingSection from "./components/PlaytestingSection/PlaytestingSecti
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+    <div className="flex-col min-h-screen items-center justify-center bg-gray-100">
       <PlaytestingSection />
         <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind! And goodmorning Vietnam!</h1>
     </div>
