8a187adfb16c6a50c15986fe9328f04ce11d1efa
Endorze <alexyurihallgren@hotmail.com>
2025-06-05 19:11:07 +0200

    fix: Added gradient line that was missing

--- src/app/components/BlogHeroSection/BlogHeroSection.tsx
+++ src/app/components/BlogHeroSection/BlogHeroSection.tsx
@@ -3,7 +3,7 @@ import H3Title from "../H3Title/H3Title";
 
 const BlogHeroSection = () => {
     return (
-        <section>
+        <section className="relative">
             <div className="hidden lg:flex bg-[url('/images/sectionbackgrounds/whisperscathedral.png')] bg-cover bg-center w-full text-center text-white justify-center relative pt-36 pb-36">
                 <div className="flex flex-col gap-6">
                     <H2Title text="WHISPERS FROM THE CATHEDRAL" size="48px"/>
@@ -18,6 +18,7 @@ const BlogHeroSection = () => {
                     <H3Title text="Latest news straight from YAHWEH'S Cathedral" size="24px"/>
                 </div>
             </div>
+            <img src={"/images/effects/gradientline.png"} className='absolute bottom-[0] left-1/2 -translate-x-1/2' />
         </section>
     )
 }
