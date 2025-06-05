5d4696b4724ca20a79c9889f63e5319f89588102
Endorze <alexyurihallgren@hotmail.com>
2025-06-05 15:47:56 +0200

    Pause

--- src/app/components/PlaytestingSection/PlaytestingSection.tsx
+++ src/app/components/PlaytestingSection/PlaytestingSection.tsx
@@ -1,10 +1,11 @@
+import H2Title from "../H2Title/H2Title";
 import PlayButton from "../PlayButton/PlayButton";
 
 const PlaytestingSection = () => {
     return (
         <div className="flex bg-[url('/images/playtesting/cathedralgameplay.png')] bg-cover bg-center h-128 w-full">
             <div>
-                <h2 className="text-white">PLAYTESTING</h2>
+                <H2Title text="Playtesting" size="96px"/>
                 <h3 className="text-white">NOW IN ALPHA</h3>
                 <h4>Early descent into Yahweh's Cathedral has begun...</h4>
                 <p>We’re currently looking for fearless souls to help test the game. Your feedback shapes the world — and perhaps your survival in it.</p>
