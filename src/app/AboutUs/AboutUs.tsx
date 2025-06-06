668af71a195effb27c0b414572396eda720f71be
Moon2024-light <brite_moon2008@yahoo.com>
2025-06-06 19:15:29 +0200

    fixed

---
+++ src/app/AboutUs/AboutUs.tsx
@@ -0,0 +1,10 @@
+import AboutSection from '../components/AboutSection/AboutSection';
+
+export default function About() {
+  return (
+    <main className="min-h-screen p-10 bg-gray-100">
+      <h1 className="text-4xl font-bold mb-10">About Us</h1>
+      <AboutSection />
+    </main>
+  );
+}
