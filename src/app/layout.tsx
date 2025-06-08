5325057a486044df74cbc2e041e4be69da0143e8
Moshira89 <mushiraaljak@example.com>
2025-06-08 22:21:43 +0200

    Fix folder name casing to HeroSection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { IM_Fell_English_SC, Germania_One } from "next/font/google";
 import "./globals.css";
-import HeroSection from "./components/HeroSection";
+import HeroSection from "./components/Herosection";
 import Header from "./components/Header";
 import Footer from "./components/Footer/Footer";
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 
-import HeroSection from "./components/HeroSection";
+import HeroSection from "./components/Herosection";
 import H2Title from "./components/H2Title/H2Title";
 import H3Title from "./components/H3Title/H3Title";
 import SupporterPack from "./components/ 006-Supporterpack";
