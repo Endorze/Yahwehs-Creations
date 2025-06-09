6322073e17483dac53747830fecddae9b3c42190
Moshira89 <mushiraaljak@example.com>
2025-06-09 19:46:31 +0200

    WIP: System Requirements layout and image placement

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,9 @@
+import SystemRequirements from "../components/system-requirements/system-requirements";
+
+export default function BlogPage() {
+  return (
+    <main className="relative">
+      <SystemRequirements />
+    </main>
+  );
+}
