5460580792a3f418ff60e66cfb784d353ce850ed
andrew <andrewkalumba29@gmail.com>
2025-06-09 22:41:29 +0200

    updated fullnews page

--- src/app/fullNews/[news]/page.tsx
+++ src/app/fullNews/[news]/page.tsx
@@ -1,7 +1,6 @@
 import Button from "@/app/components/Button"
 import { newsItems } from "@/data/news"
 
-
 interface NewsDetailsProps {
     params: { news: string }
 }
@@ -14,9 +13,9 @@ const NewsDetail = async ({ params }: NewsDetailsProps) => {
     return (
         <div className="bg-[#1B1B1B] text-white min-h-screen p-8 flex flex-col justify-center items-center">
             {newsData ?
-                <div className="border rounded-b-2xl w-full md:w-3/5 ">
+                <div className="rounded-b-2xl w-full md:w-3/5 ">
                     <div className="flex flex-col justify-center items-center">
-                        <img src={newsData.image} alt={newsData.title} className="w-full object-contain" />
+                        <img src={newsData.image} alt={newsData.title} className="borderRadius: '50%', border: '1px solid #fff', width: '100px', height: 'auto'," />
                     </div>
                     <div className="text-left p-[16px]">
                         <h1 className="text-3xl font-bold mb-4">{newsData.title}</h1>
@@ -24,10 +23,11 @@ const NewsDetail = async ({ params }: NewsDetailsProps) => {
                         <p className="mb-4">{newsData.type}</p>
                         <p className="leading-relaxed">{newsData.description || "No description provided."}</p>
                     </div>
+                    <div className="text-left p-[16px] pb-16 uppercase"><Button text="back" href={"/blog"} /></div>
                 </div>
                 : <div>"no data found"</div>
             }
-            <div className="flex items-center justify-center p-6 pb-16 uppercase"><Button text="back" href={"/"} /></div>
+            
         </div>
     )
 }
