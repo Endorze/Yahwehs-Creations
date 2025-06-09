29c01162b2cdfcb9ee1ae0b4b03b15a92f8570be
vineetha <vineethaveerapu4@gmail.com>
2025-06-09 16:23:45 +0200

    adding component offical media

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,15 @@
+import OfficialMediaSection from "../components/OfficialMediaSection/OfficialMediaSection";
+
 export default function BlogPage() {
   return (
+    <>
     <section className="p-8">
       <h1 className="text-4xl font-bold mb-4">Blog Page</h1>
       <p className="text-lg text-gray-700">
         Welcome to our Blog page. Here you'll find information about our mission, team, and story.
       </p>
     </section>
+    <OfficialMediaSection/>
+    </>
   );
 }
---
+++ src/app/components/OfficialMediaSection/OfficialMediaSection.tsx
@@ -0,0 +1,9 @@
+const OfficialMediaSection = () => {
+    return (
+        <div className="">
+            <h2 className="text-2xl font-bold">Official Media Section</h2>
+        </div>
+    )
+}
+
+export default OfficialMediaSection;
