d0e3875d313e3c8c526d3a586e5a8221af128cbd
andrew <andrewkalumba29@gmail.com>
2025-06-09 16:39:35 +0200

    removed params error

--- src/app/fullNews/[news]/page.tsx
+++ src/app/fullNews/[news]/page.tsx
@@ -7,16 +7,17 @@ interface NewsDetailsProps {
 }
 
 const NewsDetail = async ({ params }: NewsDetailsProps) => {
+    params = await params
     const newsId = parseInt(params.news)
-    const newsData = newsItems.find(item => item.id === newsId)
+    const newsData = newsItems[newsId]
 
     return (
         <div className="bg-[#1B1B1B] text-white min-h-screen p-8 flex flex-col justify-center items-center">
             {newsData ?
                 <div className="border rounded-b-2xl w-full md:w-3/5 ">
-                    <div className="flex flex-col justify-center items-center"> 
-                        <img src={newsData.image} alt={newsData.title}  className="w-full object-contain" /> 
-                        </div>
+                    <div className="flex flex-col justify-center items-center">
+                        <img src={newsData.image} alt={newsData.title} className="w-full object-contain" />
+                    </div>
                     <div className="text-left p-[16px]">
                         <h1 className="text-3xl font-bold mb-4">{newsData.title}</h1>
                         <p className="text-[#e6a54c] mb-2">{newsData.date}</p>
