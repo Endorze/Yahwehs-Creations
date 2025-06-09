dabac9e19f1d874604485527b4754f226f463a6c
vineetha <vineethaveerapu4@gmail.com>
2025-06-09 13:21:11 +0200

    Adding CreditsAboutUs component and integrate into AboutUs page

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -1,10 +1,15 @@
+import CreditsAboutUs from "../components/CreditsAboutUs";
+
 export default function AboutUsPage() {
   return (
+    <>
     <section className="p-8">
       <h1 className="text-4xl font-bold mb-4">About Us</h1>
       <p className="text-lg text-gray-700">
         Welcome to our About Us page. Here you'll find information about our mission, team, and story.
       </p>
     </section>
+    <CreditsAboutUs />
+    </>
   );
 }
