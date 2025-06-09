285449be01b52b00af4bf8f9d65aec729e5ad739
andrew <andrewkalumba29@gmail.com>
2025-06-09 22:38:18 +0200

    Added NewDetails to blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,11 @@
+import RecentNews from "../components/017-RecentNews";
 import SystemRequirements from "../components/system-requirements/system-requirements";
 
 export default function BlogPage() {
   return (
     <main className="relative">
       <SystemRequirements />
+      <RecentNews/>
     </main>
   );
 }
--- src/app/fullnews/[news]/page.tsx
+++ src/app/fullnews/[news]/page.tsx
@@ -13,9 +13,10 @@ const NewsDetail = async ({ params }: NewsDetailsProps) => {
     return (
         <div className="bg-[#1B1B1B] text-white min-h-screen p-8 flex flex-col justify-center items-center">
             {newsData ?
-                <div className="border rounded-b-2xl w-full md:w-3/5 ">
+                <div className="rounded-b-2xl w-full md:w-3/5 ">
                     <div className="flex flex-col justify-center items-center">
-                        <img src={newsData.image} alt={newsData.title} className="w-full object-contain" />
+                        <img src={newsData.image} alt={newsData.title} className="borderRadius: '50%',
+  border: '1px solid #fff', width: '100px', height: 'auto'," />
                     </div>
                     <div className="text-left p-[16px]">
                         <h1 className="text-3xl font-bold mb-4">{newsData.title}</h1>
@@ -23,10 +24,11 @@ const NewsDetail = async ({ params }: NewsDetailsProps) => {
                         <p className="mb-4">{newsData.type}</p>
                         <p className="leading-relaxed">{newsData.description || "No description provided."}</p>
                     </div>
+                    <div className="text-left p-[16px] pb-16 uppercase"><Button text="back" href={"/blog"} /></div>
                 </div>
                 : <div>"no data found"</div>
             }
-            <div className="flex items-center justify-center p-6 pb-16 uppercase"><Button text="back" href={"/"} /></div>
+            
         </div>
     )
 }
