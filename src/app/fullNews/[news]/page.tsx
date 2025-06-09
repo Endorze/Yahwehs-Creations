667a9219790a48b8588d1d181aa8d45c5a13baa9
andrew <andrewkalumba29@gmail.com>
2025-06-10 00:45:52 +0200

    width to bottom image to w-screen

--- src/app/fullNews/[news]/page.tsx
+++ src/app/fullNews/[news]/page.tsx
@@ -11,31 +11,38 @@ const NewsDetail = async ({ params }: NewsDetailsProps) => {
     const newsData = newsItems.find(item => item.id === newsId)
 
     return (
-        <div className="bg-[#1B1B1B] text-white min-h-screen p-8 flex flex-col justify-center items-center">
-            {newsData ? (
-                <div className="w-full md:w-[50%] bg-black overflow-hidden">
-                    <div className="w-full h-[60vh]">
-                        <img src={newsData.image} alt={newsData.title} className="w-full h-full object-fill" />
-                    </div>
+        <>
+            <div className="bg-[#1B1B1B] text-white min-h-screen p-8 flex flex-col justify-center items-center">
+                {newsData ? (
+                    <div className="w-full md:w-[50%] bg-black overflow-hidden">
+                        <div className="w-full h-[60vh]">
+                            <img src={newsData.image} alt={newsData.title} className="w-full h-full object-fill" />
+                        </div>
 
-                    <div className="text-left p-4">
-                        <h1 className="text-3xl font-bold mb-4">{newsData.title}</h1>
-                        <p className="text-[#e6a54c] mb-2">{newsData.date}</p>
-                        <p className="mb-4">{newsData.type}</p>
-                        <p className="leading-relaxed">{newsData.description}</p>
-                    </div>
+                        <div className="text-left p-4">
+                            <h1 className="text-3xl font-bold mb-4">{newsData.title}</h1>
+                            <p className="text-[#e6a54c] mb-2">{newsData.date}</p>
+                            <p className="mb-4">{newsData.type}</p>
+                            <p className="leading-relaxed">{newsData.description}</p>
+                        </div>
 
-                    <div className="text-left p-4 pb-16 uppercase">
-                        <Button text="back" href="/blog" />
+                        <div className="text-left p-4 pb-16 uppercase">
+                            <Button text="back" href="/blog" />
+                        </div>
                     </div>
-                </div>
-            ) : (
-                <div>No data found</div>
-            )}
-            <div className="flex items-center justify-center w-full -mt-10 p-[3px]">
-                <img src="/images/effects/gradientline.png" alt="Gradient Line" />
+                ) : (
+                    <div>No data found</div>
+                )}
+
             </div>
-        </div>
+            <div className="w-screen -mt-10">
+                <img
+                    src="/images/effects/gradientline.png"
+                    alt="Gradient Line"
+                    className="w-full object-cover"
+                />
+            </div>
+        </>
     )
 }
 
