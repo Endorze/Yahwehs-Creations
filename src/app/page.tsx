894d62782d6008f779e6a26b58a21f433b197e45
andrew <andrewkalumba29@gmail.com>
2025-06-09 12:15:22 +0200

    added removed commits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import SupporterPack from "./components/ 006-Supporterpack";
 import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
     <div >
       <HeroSection />
+      <SupporterPack/>
 
     </div>
   );
