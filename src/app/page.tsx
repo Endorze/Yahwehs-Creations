b5ceff46bb149023ba8f1e7d1f5b03373d310f9a
Saba Waheed <saba_sh27@hotmail.com>
2025-06-10 14:15:03 +0200

    remove h1 from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,6 @@ export default function Home() {
     <div >
       <Herosection />
       <SupporterPack/>
-
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-        <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind! And goodmorning Vietnam!</h1>
-
-    </div>
     </div>
   );
 }
