740d20f0790a17f797241c885201bfdb8a496169
Moshira89 <mushiraaljak@example.com>
2025-06-10 17:53:05 +0200

    update system

--- src/app/components/021-systemRequirements/index.tsx
+++ src/app/components/021-systemRequirements/index.tsx
@@ -2,24 +2,23 @@ import Image from "next/image";
 
 export default function SystemRequirements() {
   return (
-    <section className="relative w-full min-h-[1150px] bg-black text-white">
-      <h1
-        className="absolute top-10 left-10 sm:top-8 sm:left-48 text-[32px] sm:text-[48px] leading-[40px] sm:leading-[56px] font-[Germania_One] z-10"
-        style={{ fontWeight: 400 }}
-      >
-        SYSTEM REQUIREMENTS
-      </h1>
+    <section className="flex flex-col justify-center items-center min-h-screen bg-black text-white">
+      <div className="flex flex-col items-center">
+        <h1 className="text-white-500 text-[32px] sm:text-[48px] font-[Germania_One] mb-8" style={{ fontWeight: 400 }}>
+          SYSTEM REQUIREMENTS
+        </h1>
 
-      <div className="absolute top-[160px] left-[5%] w-[90%] max-w-[1122px] h-[60vh] sm:top-[216px] sm:left-[187px] sm:w-[1122px] sm:h-[748px]">
-        <Image
-          src="/images/image5.png"
-          alt="System Requirements Image"
-          fill
-          className="object-cover"
-        />
+        <div className="w-[90vw] max-w-[1122px] h-[60vh] sm:h-[748px] relative">
+          <Image
+            src="/images/image5.png"
+            alt="System Requirements Image"
+            fill
+            className="object-cover"
+          />
+        </div>
       </div>
 
-      <div className="absolute w-[80%] max-w-[633px] h-[34px] left-1/2 -translate-x-1/2 bottom-8 sm:top-[1020px] sm:left-1/2 sm:-translate-x-1/2">
+      <div className="relative w-[80%] max-w-[633px] h-[34px] mt-16">
         <Image
           src="/images/effects/gradientline.png"
           alt="Gradient Line"
