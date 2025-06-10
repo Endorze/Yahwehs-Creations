eccbf925642e8e135238a51977a6059067d5ae61
Endorze <alexyurihallgren@hotmail.com>
2025-06-10 11:10:24 +0200

    feat: Added back my components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import SupporterPack from "./components/ 006-Supporterpack";
+import BlogHeroSection from "./components/BlogHeroSection/BlogHeroSection";
+import FAQSection from "./components/FAQSection/FAQSection";
 import HeroSection from "./components/HeroSection";
 
 export default function Home() {
@@ -6,7 +8,8 @@ export default function Home() {
     <div >
       <HeroSection />
       <SupporterPack/>
-
+      <BlogHeroSection />
+      <FAQSection />
     </div>
   );
 }
