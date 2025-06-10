7f04b3715943c13663a8bf120397cb1c88c1a183
andrew <andrewkalumba29@gmail.com>
2025-06-10 18:06:42 +0200

    fixed image

--- src/app/fullnews/[news]/page.tsx
+++ src/app/fullnews/[news]/page.tsx
@@ -1,8 +1,8 @@
 import Button from "@/app/components/Button"
 import { newsItems } from "@/data/news"
 
-const NewsDetail = async ({ params }: {params: Promise<{news:string}>}) => {
-   const {news} = await params
+const NewsDetail = async ({ params }: { params: Promise<{ news: string }> }) => {
+    const { news } = await params
     const newsId = parseInt(news)
     const newsData = newsItems.find(item => item.id === newsId)
 
@@ -11,9 +11,7 @@ const NewsDetail = async ({ params }: {params: Promise<{news:string}>}) => {
             <div className="bg-[#1B1B1B] text-white min-h-screen p-8 flex flex-col justify-center items-center">
                 {newsData ? (
                     <div className="w-full lg:w-[50%] bg-black overflow-hidden">
-                        <div className="w-full h-[60vh]">
-                            <img src={newsData.image} alt={newsData.title} className="w-full h-full object-fill" />
-                        </div>
+                        <img src={newsData.image} alt={newsData.title} className="w-full h-[70%] md:h-full object-fill" />
 
                         <div className="text-left p-4">
                             <h1 className="text-3xl font-bold mb-4">{newsData.title}</h1>
