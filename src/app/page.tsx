b05de56a23b7779b20969221c55327e4f4763ce9
Endorze <alexyurihallgren@hotmail.com>
2025-06-10 14:13:42 +0200

    fix: Fixed section for desktop, phone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,10 @@
+import OurCreations from "./components/OurCreations/OurCreations";
 
-import H2Title from "./components/H2Title/H2Title";
-import H3Title from "./components/H3Title/H3Title";
-import PlayButton from "./components/PlayButton/PlayButton";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col min-h-screen items-center justify-center">
-        <PlayButton />
-        <h1 className="text-4xl font-bold text-blue-600">
-          Hello Yahweh's Creations fans
-        </h1>
-        <H2Title text="Test title" size="72px"/>
-        <H3Title text="Test title" size="32px"/>
-      </div>
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-        <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind! And goodmorning Vietnam!</h1>
+      <div className="flex flex-col min-h-screen">
+        <OurCreations />
     </div>
  </>
   );
