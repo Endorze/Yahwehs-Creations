8421658ed020fdaa577a5c3fc0415d2a523c153e
Saba Waheed <saba_sh27@hotmail.com>
2025-06-10 14:10:43 +0200

    solved conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import Herosection from "./components/Herosection";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
     <div >
       <Herosection />
       <SupporterPack/>
-=======
+
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
         <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind! And goodmorning Vietnam!</h1>
->>>>>>> 010-Feature/Trailer-section
+
+    </div>
     </div>
   );
 }
