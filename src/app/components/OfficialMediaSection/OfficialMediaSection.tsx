14d693c386a34434754c0766709740bdf0cdc6b2
vineetha <vineethaveerapu4@gmail.com>
2025-06-10 21:26:26 +0200

    fixing margin

--- src/app/components/OfficialMediaSection/OfficialMediaSection.tsx
+++ src/app/components/OfficialMediaSection/OfficialMediaSection.tsx
@@ -8,9 +8,10 @@ const OfficialMediaSection = () => {
     <div className="bg-[url('/images/sectionbackgrounds/officialmedia.png')] bg-cover bg-center bg-no-repeat">
 
         <div className="md:py-20 md:px-40 py-4 px-4 uppercase ">
+          <div className="mb-10">
           <H2Title text="Official Media " size="48px" />
           <H3Title text="YAHWEH REPRESENTS " size="24px" />
-
+          </div>
           <div className="grid grid-cols-3 gap-4 justify-center items-center">
            <OfficialMediaCard/>
            <OfficialMediaCard/>
@@ -23,6 +24,8 @@ const OfficialMediaSection = () => {
            <OfficialMediaCard/>
           </div>
         </div>
+
+        <div></div>
       </div>
 
   );
