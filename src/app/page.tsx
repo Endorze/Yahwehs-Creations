dd2c1e2e8257698dbadcfda5af070b54e74db9d3
Yasmine Borghol <yesmine.borghol@gmail.com>
2025-06-11 13:10:45 +0100

    added component in the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
         <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind! And goodmorning Vietnam!</h1>
     </div>
+    <LatestNews />
  </>
   );
 
