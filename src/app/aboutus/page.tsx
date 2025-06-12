bb8f62fe50677d966fd5a6ae7f20eca1a08d52db
Saba Waheed <saba_sh27@hotmail.com>
2025-06-12 13:26:00 +0200

    fixed

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -1,4 +1,5 @@
 
+import TrailerSection from '../components/010-TrailerSection';
 import AboutSection from '../components/AboutSection/AboutSection';
 import CreditsAboutUs from "../components/CreditsAboutUs";
 
@@ -6,12 +7,9 @@ export default function AboutUsPage() {
   return (
     <>
       <main className="min-h-screen bg-white">
-        <section className="p-8">
-          <h1 className="text-4xl font-bold mb-4">About Us</h1>
-          <p className="text-lg text-gray-700">
-            Welcome to our About Us page. Here you'll find information about our mission, team, and story.
-          </p>
-        </section>
+     
+     
+        <TrailerSection />
         <CreditsAboutUs />
         <AboutSection />
       </main>
