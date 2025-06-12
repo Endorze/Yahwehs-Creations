df1ae1bb6028d2b66ba6882565b55b9235c35a5a
Saba Waheed <saba_sh27@hotmail.com>
2025-06-12 16:34:38 +0200

    created new branch

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,10 @@
+import MeetTheCreatures from "../components/MeetTheCreatures";
+
+
 export default function BlogPage() {
   return (
-    <section className="p-8">
-      <h1 className="text-4xl font-bold mb-4">Blog Page</h1>
-      <p className="text-lg text-gray-700">
-        Welcome to our Blog page. Here you'll find information about our mission, team, and story.
-      </p>
+    <section >
+     <MeetTheCreatures />
     </section>
   );
 }
