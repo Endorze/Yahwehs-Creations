1006fdfa1dc120568aa18528aed2b68f9e5c8d6c
Endorze <alexyurihallgren@hotmail.com>
2025-06-12 11:37:27 +0200

    fix: Added text

--- src/app/components/021-AlexTestSystemRequirements/index.tsx
+++ src/app/components/021-AlexTestSystemRequirements/index.tsx
@@ -5,6 +5,7 @@ const AlexTestSystemRequirements = () => {
         <div className="hidden lg:flex bg-black flex flex-col justify-center items-center pt-12 pb-24">
             <div className="mb-12">
             <H2Title text="SYSTEM REQUIREMENTS" size="72px" />
+            <p>Made by Alex, not meant to be code reviewed</p>
             </div>
             <div>
                 <img src={"/images/systemreq.png"} className="w-full max-w-screen-lg h-auto px-4 z1" />
