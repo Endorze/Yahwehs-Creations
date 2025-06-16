02731fdbf61380cdb8b0a264ca7f416298fec809
Endorze <alexyurihallgren@hotmail.com>
2025-06-16 12:18:49 +0200

    feat: Built the pages using the finished sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,18 @@
 
 import SupporterPack from "./components/ 006-Supporterpack";
-import H2Title from "./components/H2Title/H2Title";
-import H3Title from "./components/H3Title/H3Title";
-import PlayButton from "./components/PlayButton/PlayButton";
 import LatestNews from "./components/LatestNews/index"
+import HeroSection from "./components/Herosection";
+import OurCreations from "./components/OurCreations/OurCreations";
 
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col min-h-screen items-center justify-center">
-        <PlayButton />
-        <h1 className="text-4xl font-bold text-blue-600">
-          Hello Yahweh's Creations fans
-        </h1>
-        <H2Title text="Test title" size="72px"/>
-        <H3Title text="Test title" size="32px"/>
+      <div className="flex flex-col min-h-screen justify-center">
+        <HeroSection />
+        <LatestNews />
+        <SupporterPack />
+        <OurCreations />
       </div>
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-        <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind! And goodmorning Vietnam!</h1>
-    </div>
-    <SupporterPack/>
-    <LatestNews />
-    
- </>
+    </>
   );
-
 }
