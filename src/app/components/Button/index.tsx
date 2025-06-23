dd791605450779aac99f0d03ca744d1fc2215d35
andrew <andrewkalumba29@gmail.com>
2025-06-23 17:17:32 +0200

    Added a boolean

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -1,32 +1,39 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 interface ButtonProps {
   text: string;
   href?: string;
-  onClick?: () => void;
+  back?: boolean;
 }
 
 const Button = (props: ButtonProps) => {
+  const { text, href, back } = props;
 
-  const { text, href, onClick } = props;
-  const ClassName = "bg-[url('/images/orangebutton.png')] bg-cover bg-center bg-no-repeat text-xl text-white aspect-245/49 text-center hover:shadow-[0_0_10px_5px_rgba(255,165,0,0.5)] hover:scale-105 transition-all duration-300 cursor-pointer font-bold p-2 hover:bg-[rgba(255,165,0,0.5)]"
-
-  if (href) {
-    return (
-
-      <Link
-        href={href} className={ClassName}> {text}
-      </Link>
-    )
-  } else {
-    return (
-
-      <button onClick={onClick} className={ClassName}>
-        {text}
-      </button>
-    )
+  const router = useRouter();
+  const handleClick = () => {
+    router.back()
   }
 
+  const Styling =
+    "bg-[url('/images/orangebutton.png')] bg-cover bg-center bg-no-repeat text-xl text-white aspect-245/49 text-center hover:shadow-[0_0_10px_5px_rgba(255,165,0,0.5)] hover:scale-105 transition-all duration-300 cursor-pointer font-bold p-2 hover:bg-[rgba(255,165,0,0.5)]";
+
+  if (back) {
+    return (
+      <button onClick={handleClick} className={Styling}>
+        {text}
+      </button>
+    );
+  } else if (href) {
+    return (
+      <Link href={href} className={Styling}>
+        {text}
+      </Link>
+    );
+  } else
+    return <div className={Styling}>{text}</div>;
 };
 
-export default Button
+export default Button;
