de9dbe3784e198296b124ab00318070bc64505cd
andrew <andrewkalumba29@gmail.com>
2025-06-23 16:00:40 +0200

    fixed Button

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -2,20 +2,31 @@ import Link from "next/link";
 
 interface ButtonProps {
   text: string;
-  href: string;
+  href?: string;
+  onClick?: () => void;
 }
 
 const Button = (props: ButtonProps) => {
-  // This are default values for the button
-  const { text, href } = props;
-  return (
-    <Link
-      href={href}
-      className="bg-[url('/images/orangebutton.png')] bg-cover bg-center bg-no-repeat text-xl text-white aspect-245/49 text-center hover:shadow-[0_0_10px_5px_rgba(255,165,0,0.5)] hover:scale-105 transition-all duration-300 cursor-pointer font-bold p-2 hover:bg-[rgba(255,165,0,0.5)]"
-    >
-      {text}
-    </Link>
-  );
+
+  const { text, href, onClick } = props;
+  const ClassName = "bg-[url('/images/orangebutton.png')] bg-cover bg-center bg-no-repeat text-xl text-white aspect-245/49 text-center hover:shadow-[0_0_10px_5px_rgba(255,165,0,0.5)] hover:scale-105 transition-all duration-300 cursor-pointer font-bold p-2 hover:bg-[rgba(255,165,0,0.5)]"
+
+  if (href) {
+    return (
+
+      <Link
+        href={href} className={ClassName}> {text}
+      </Link>
+    )
+  } else {
+    return (
+
+      <button onClick={onClick} className={ClassName}>
+        {text}
+      </button>
+    )
+  }
+
 };
 
-export default Button;
+export default Button
