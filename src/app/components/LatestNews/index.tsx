e21d3b0b393a7093a69b049708340441c1c1b985
Moshira89 <mushiraaljak@example.com>
2025-06-24 18:03:13 +0200

    removed allnews and add Ok'Rhams creations

--- src/app/components/LatestNews/index.tsx
+++ src/app/components/LatestNews/index.tsx
@@ -40,16 +40,16 @@ export default function LatestNews() {
               LATEST NEWS
             </h2>
             <p className="text-[#808080] text-sm" style={{ fontFamily: 'Germania One, cursive' }}>
-              YAHWEH'S CREATIONS
+              Ok'Rhams CREATIONS
             </p>
           </div>
-          <button
+          {/* <button
             onClick={handleAllNewsClick}
             className="text-[#808080] hover:text-[#e6a54c] transition-colors duration-300 text-sm cursor-pointer"
             style={{ fontFamily: 'Germania One, cursive' }}
           >
             ALL NEWS
-          </button>
+          </button> */}
         </div>
 
         <div className="flex flex-col gap-4 pb-12">
